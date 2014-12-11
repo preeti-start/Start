8a6a03476bc6557a2e4f50cb1fac7a9bfd4d1b05
sushil-daffo <sushil-daffo@github.com>
2014-12-11 16:44:41 +0530

    ok

---
+++ Servercode/Functions.js
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sushil
+ * Date: 11/12/14
+ * Time: 3:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// var exports = module.exports = {};
+
+
+
+exports.sayHelloInEnglish = function() {
+    console.log("sayHelloInEnglish called>>>>>>")
+    return "HELLO";
+};
+
+exports.sayHelloInSpanish = function() {
+    console.log("sayHelloInSpanish called>>>>>>")
+    return "Hola";
+};
